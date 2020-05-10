3317c7a48db9d8c68e4d81e68b4c6013058e7381
Charles Baynham <charles.baynham@npl.co.uk>
2020-05-10 15:08:16 +0100

    Abandon attempt to alter enum class outside of definition
    See https://stackoverflow.com/questions/871264/what-does-operator-must-be-a-non-static-member-mean

--- test/test_write.cpp
+++ test/test_write.cpp
@@ -4,11 +4,6 @@
 #include "RAM_storage.h"
 
 
-// Just for the tests, allow OFSF::result objects to be cast to ints
-operator int( OSFS::result &r ) {
-  return int(r);
-}
-
 // Unit tests for writing files
 
 unittest_setup() {
